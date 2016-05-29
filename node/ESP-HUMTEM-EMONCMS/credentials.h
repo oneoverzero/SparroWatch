101e4228b7dd1ac41a0825e0808e9b32341b3bd8
Luis Correia <buga@loide.net>
2016-05-29 19:59:15 +0100

    Add test program for posting humidity and temperature values to emoncms

---
+++ node/ESP-HUMTEM-EMONCMS/credentials.h
@@ -0,0 +1,16 @@
+/*
+   Personal definitions
+*/
+
+#define ssid     "your_ap_ssid"
+#define password "your_ap_password"
+#define host     "emoncms.org"
+#define apikey   "your_apikey"
+#define node     "1"
+
+/*
+   Helper defines
+*/
+#define baseurl  "/input/post.json?apikey=" + String(apikey) + "&node=" + node + "&json="
+
+
